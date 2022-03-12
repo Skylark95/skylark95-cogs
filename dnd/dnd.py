6daca0e092746ac275549071ec008930d7ab5689
Derek Cochran <derek@skylark95.com>
2022-03-11 21:04:58 -0600

    add classes to spell command

--- dnd/dnd.py
+++ dnd/dnd.py
@@ -21,15 +21,18 @@ class Dnd(commands.Cog):
                     json = await resp.json()
                     name = json.get('name')
                     desc = '\n\n'.join(json.get('desc', []))
-                    description = desc
+
                     higher_level = json.get('higher_level', [])
                     if len(higher_level) > 0:
-                        description = description + '\n\n**At Higher Levels.** ' + '\n\n'.join(higher_level)
+                        desc = desc + '\n\n**At Higher Levels.** ' + '\n\n'.join(higher_level)
+
                     material = json.get('material')
                     if material != None:
-                        description = description + '\n\n**Material.** ' + material
+                        desc = desc + '\n\n**Material.** ' + material
 
-                    embed = discord.Embed(title=f'Spell: {name}', description=description, color=(await ctx.embed_colour()))
+                    classes = ', '.join(list(map(lambda c: c.get('index'), json.get('classes', []))))
+
+                    embed = discord.Embed(title=f'Spell: {name}', description=desc, color=(await ctx.embed_colour()))
                     embed.add_field(name="Level", value=json.get('level'))
                     embed.add_field(name="Casting Time", value=json.get('casting_time'))
                     embed.add_field(name="Range/Area", value=json.get('range'))
@@ -38,6 +41,7 @@ class Dnd(commands.Cog):
                     embed.add_field(name="School", value=json.get('school', {}).get('name'))
                     embed.add_field(name="Attack/Save", value=json.get('attack_type'))
                     embed.add_field(name="Damage/Effect", value=json.get('damage', {}).get('damage_type', {}).get('name'))
+                    embed.add_field(name="Classes", value=classes)
                     return await ctx.send(embed=embed)
                 elif resp.status == 404:
                     return await ctx.send(f'Could not find spell {spell}')

c5a6f0b66dd5c488ee2a882219c34cda6bf83a8f
Derek Cochran <derek@derekcochran.dev>
2022-04-18 22:16:59 -0500

    change wingspan expansion release time

--- wingspan/wingspan.py
+++ wingspan/wingspan.py
@@ -12,6 +12,6 @@ class Wingspan(commands.Cog):
     async def expansion(self, ctx: commands.Context):
         """Time until Wingspan European expansion is released"""
         date_today = datetime.now(timezone.utc)
-        date_release = datetime(2022, 5, 2, 0, 0, 0, 0, timezone.utc)
+        date_release = datetime(2022, 5, 2, 5, 0, 0, 0, timezone.utc)
         time_until = date_release - date_today
         return await ctx.send(f'Wingspan: European Expansion will be released in **{time_until.days} days**')
